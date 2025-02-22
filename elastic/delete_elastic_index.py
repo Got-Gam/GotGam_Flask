5c1cbb82270c8ad8a9efff100cf32aeb7b97e81c
BeigeHome <kima606@gmail.com>
2025-02-23 00:03:58 +0900

    2025.02.22 강휘 엘라스틱 서치 인덱스 제거

---
+++ elastic/delete_elastic_index.py
@@ -0,0 +1,23 @@
+from elasticsearch import Elasticsearch
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+elastic_pwd = os.getenv("ELASTIC_PASSWORD")
+
+def delete_elasticsearch_index(index_name):
+    try:
+        es = Elasticsearch("http://localhost:9200",
+                           basic_auth=('elastic', elastic_pwd))
+
+        if es.indices.exists(index=index_name):
+            es.indices.delete(index=index_name)
+            print("Delete index: " + index_name)
+        else:
+            print("Index does not exist: " + index_name)
+
+    except Exception as e:
+        print(e)
+
+delete_elasticsearch_index("tour_spots")
