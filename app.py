3ff3be7c9f4b40c2e150b456d36cc64fbed7b3d0
BeigeSeason <kima606@gmail.com>
2025-03-10 17:48:31 +0900

    2025.03.10 강휘 catboost 시험

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from elastic.delete_elastic_index import delete_elasticsearch_index
 from elastic.diary_elastic import create_diary_index
 from elastic.tour_to_elastic import send_to_elastic
 
-
 delete_elasticsearch_index("tour_spots")
-delete_elasticsearch_index("diary") # 다이어리 인덱스 초기화
+delete_elasticsearch_index("diary")  # 다이어리 인덱스 초기화
 send_to_elastic("./tour_spot_info2.json")
 create_diary_index()
+
