721d22ba904958d647f5224119045f32406b132b
BeigeHome <kima606@gmail.com>
2025-04-17 20:38:32 +0900

    25.04.17

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def initialize():
     try:
         delete_elasticsearch_index("tour_spots")
         delete_elasticsearch_index("diary")  # 다이어리 인덱스 초기화
-        send_to_elastic("./tour_spot_info2.json")
+        send_to_elastic("tour_spot_info.json")
         create_diary_index()
         return jsonify({'status': 'success', 'message': 'Server initialized successfully'})
     except Exception as e:
