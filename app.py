016f9043f9a08878a2f08cdeb6176e87b464f943
BeigeSeason <kima606@gmail.com>
2025-04-17 22:11:50 +0900

    2025.04.17 git 테스트

--- app.py
+++ app.py
@@ -71,16 +71,6 @@ def get_recommendations():
         return jsonify({'status': 'error', 'message': str(e)})
 
 
-# @app.route('/server-initialize', methods=['POST'])
-# def initialize():
-#     try:
-#         delete_elasticsearch_index("tour_spots")
-#         delete_elasticsearch_index("diary")  # 다이어리 인덱스 초기화
-#         send_to_elastic("tour_spot_info.json")
-#         create_diary_index()
-#         return jsonify({'status': 'success', 'message': 'Server initialized successfully'})
-#     except Exception as e:
-#         return jsonify({'status': 'error', 'message': str(e)}), 500
 
 
 @app.route('/test', methods=['GET'])
